0cb73ab318f6dafaa034360b76c5287fb876089c
lbatalha <lfrb25@gmail.com>
2015-06-02 05:01:57 +0100

    list repo content

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-
+#!/usr/bin/python3
 import os
-from flask import Flask
+import pygit2
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
+root = '/home/lbatalha/pgit_data/repos/'
 
 @app.route('/')
 def repo_list():
-	return '\n'.join(os.listdir()) 
-		
+	return render_template('root.html', dirlist=os.listdir(root), root=root)
+
+@app.route('/<repo_name>')
+def dir_list(repo_name):
+	path = root + repo_name
+	repo = pygit2.Repository(path)
+	commit = repo.revparse_single('master')
+	list = [];
+	for entry in commit.tree:
+		#entry.id, entry.name
+		#obj = repo.get(entry.id)
+		#list.append(obj.read_raw())
+		files.append(entry.name)
+
+	return render_template('repo.html', files=files, repo_name=repo_name)
+
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
