528c901e83411908f54e7347fd5f0182cf73e8f4
lbatalha <lfrb25@gmail.com>
2015-06-04 00:07:09 +0100

    traverse foldersy

--- main.py
+++ main.py
@@ -18,13 +18,12 @@ def dir_list(repo_name):
 	files = [];
 	
 	for entry in commit.tree:
-		files += entry
+		files.append(entry)
 		
 	return render_template('repo.html', files=files, repo=repo)
 
 @app.route('/<repo_name>/<path:file_path>')
 def file_contents(repo_name, file_path):
-	style = url_for('static', filename='styles/file.css')
 	path = root + repo_name
 	repo = pygit2.Repository(path)
 	commit = repo.revparse_single('master')
@@ -34,11 +33,12 @@ def file_contents(repo_name, file_path):
 		
 	if(obj.type == pygit2.GIT_OBJ_TREE):
 		files = []
+		obj = repo[obj.id]
 		for entry in obj:
-			files += entry
+			files.append(entry)
 		return render_template('path.html', file_path=file_path, files=files)
 	else:
-		return render_template('file.html', file_path=file_path, obj=obj, style=style)
+		return render_template('file.html', file_path=file_path, obj=obj)
 
 if __name__ == '__main__':
 	app.debug = True
