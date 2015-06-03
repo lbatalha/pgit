b806e01b615920c02bfa1e78afc7c479f7a894cc
lbatalha <lfrb25@gmail.com>
2015-06-03 01:37:42 +0100

    add static, css

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import os
 import pygit2
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -11,7 +11,7 @@ root = '/home/lbatalha/pgit_data/repos/'
 def repo_list():
 	return render_template('root.html', dirlist=os.listdir(root), root=root)
 
-@app.route('/<repo_name>')
+@app.route('/<repo_name>/')
 def dir_list(repo_name):
 	path = root + repo_name
 	repo = pygit2.Repository(path)
@@ -23,13 +23,17 @@ def dir_list(repo_name):
 		
 	return render_template('repo.html', files=files, repo=repo)
 
-@app.route('/<repo_name>/<file_name>')
-def file_contents(repo_name, file_name):
+@app.route('/<repo_name>/<path:file_path>')
+def file_contents(repo_name, file_path):
+	url_for('static', filename='styles/file.css')
 	path = root + repo_name
 	repo = pygit2.Repository(path)
-	obj = repo[tree[file_name]]
-	
-	return render_template('file.html', file_name=file_name, obj=obj)
+	commit = repo.revparse_single('master')
+	file_id = commit.tree[file_path].id
+	obj = repo.get(file_id)
+
+	return render_template('file.html', file_path=file_path, obj=obj)
+
 if __name__ == '__main__':
 	app.debug = True
 	app.run()
