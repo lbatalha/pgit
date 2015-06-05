4dcbab70883c7426a7cd1cc145938370c9bd01c9
lbatalha <lfrb25@gmail.com>
2015-06-05 18:34:54 +0100

    add commit log

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
+import os, sys
+from datetime import datetime
 
 import pygit2
+
+from pygit2 import GIT_SORT_TOPOLOGICAL, GIT_SORT_REVERSE
 
 from flask import Flask, render_template, url_for
 
@@ -28,7 +31,7 @@ def file_contents(repo_name, file_path=None):
 	if file_path == None:			#Check for Repo List
 		for entry in commit.tree:
 			files.append(entry)
-		return render_template('repo.html', files=files, repo=repo)
+		return render_template('repo.html', files=files, repo=repo, repo_name=repo_name)
 
 	file_id = commit.tree[file_path].id
 	obj = repo.get(file_id)
@@ -38,7 +41,7 @@ def file_contents(repo_name, file_path=None):
 		obj = repo[obj.id]
 		for entry in obj:
 			files.append(entry)
-		return render_template('path.html', file_path=file_path, files=files)
+		return render_template('path.html', repo_name=repo_name, file_path=file_path, files=files)
 	else:
 		obj = repo[obj.id]
 		content = obj.read_raw().decode('utf8')
@@ -46,6 +49,18 @@ def file_contents(repo_name, file_path=None):
 		formatter = HtmlFormatter(linenos=True, cssclass='source')
 		result = highlight(content, lexer, formatter)
 		return render_template('file.html', file_path=file_path, result=result)
+
+@app.route('/<repo_name>/commits')
+def commit_list(repo_name):
+	commit_list = []
+	path = ROOT + repo_name
+	repo = pygit2.Repository(path)
+	
+	for commit in repo.walk(repo.head.target, GIT_SORT_TOPOLOGICAL):
+		commit_list.append(commit)
+	
+	return render_template('commit_list.html', repo_name=repo_name, commit_list=commit_list)
+
 
 if __name__ == '__main__':
 	app.debug = True
