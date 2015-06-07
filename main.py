1b7f313fd9ceb58082a9224e5f915454b6234fdd
lbatalha <lfrb25@gmail.com>
2015-06-07 02:32:48 +0100

    add branching support

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+
+#########	Builtins	#########
 import os, sys
+
 from datetime import datetime
+from functools import wraps
+
+#########	External Modules	#########
 
 import pygit2
 
@@ -16,22 +22,23 @@ app = Flask(__name__)
 
 ROOT = '/home/lbatalha/pgit_data/repos/'
 
+
 @app.route('/')
 def repo_list():
 	return render_template('root.html', dirlist=os.listdir(ROOT), ROOT=ROOT)
 
-@app.route('/<repo_name>/')
-@app.route('/<repo_name>/<path:file_path>')
-def file_contents(repo_name, file_path=None):
+@app.route('/<repo_name>/<branch>')
+@app.route('/<repo_name>/<branch>/<path:file_path>')
+def file_contents(repo_name, branch='master', file_path=None):
 	files = []
 	path = ROOT + repo_name
 	repo = pygit2.Repository(path)
-	commit = repo.revparse_single('master')
+	commit = repo.revparse_single(branch)
 
 	if file_path == None:			#Check for Repo List
 		for entry in commit.tree:
 			files.append(entry)
-		return render_template('repo.html', files=files, repo=repo, repo_name=repo_name)
+		return render_template('repo.html', files=files, repo=repo, branch=branch)
 
 	file_id = commit.tree[file_path].id
 	obj = repo.get(file_id)
@@ -41,17 +48,17 @@ def file_contents(repo_name, file_path=None):
 		obj = repo[obj.id]
 		for entry in obj:
 			files.append(entry)
-		return render_template('path.html', repo_name=repo_name, file_path=file_path, files=files)
+		return render_template('path.html', repo_name=repo_name, file_path=file_path, files=files, branch=branch)
 	else:
 		obj = repo[obj.id]
 		content = obj.read_raw().decode('utf8')
 		lexer = get_lexer_for_filename(file_path, stripall=True)
 		formatter = HtmlFormatter(linenos=True, cssclass='source')
 		result = highlight(content, lexer, formatter)
-		return render_template('file.html', file_path=file_path, result=result)
+		return render_template('file.html', file_path=file_path, result=result, branch=branch)
 
-@app.route('/<repo_name>/commits')
-def commit_list(repo_name):
+@app.route('/<repo_name>/<branch>/commits')
+def commit_list(repo_name, branch='master'):
 	commit_list = []
 	path = ROOT + repo_name
 	repo = pygit2.Repository(path)
